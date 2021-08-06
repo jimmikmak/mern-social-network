e4b2ee78ea321cbe8c0e862fbde17af01082f7ee
James McCarron <mccarron.j@gmail.com>
2021-08-06 13:11:01 +1000

    Started on the GET current user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
 
-const Profile = require("../../models/User");
+const Profile = require("../../models/Profile");
 const User = require("../../models/User");
 
 // @route GET api/profile/me
@@ -25,5 +26,51 @@ router.get("/me", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+
+// @route POST api/profile
+// @desc Create or update a user profile
+// @access Private
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("status", "Status is required").isEmpty(),
+      check("skills", "Skills is required").isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // destructure the request
+    const {
+      website,
+      skills,
+      youtube,
+      twitter,
+      instagram,
+      linkedin,
+      facebook,
+      // spread the rest of the fields we don't need to check
+      ...rest
+    } = req.body;
+
+    // build a profile
+    const profileFields = {
+      user: req.user.id,
+      website:
+        website && website !== ""
+          ? normalize(website, { forceHttps: true })
+          : "",
+      skills: Array.isArray(skills)
+        ? skills
+        : skills.split(",").map((skill) => " " + skill.trim()),
+      ...rest,
+    };
+  }
+);
 
 module.exports = router;
