abb31f17ccb652eaa6584766cee0f2059f44e91c
James McCarron <mccarron.j@gmail.com>
2021-08-06 13:35:48 +1000

    Changes to profile object

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -35,8 +35,8 @@ router.post(
   [
     auth,
     [
-      check("status", "Status is required").isEmpty(),
-      check("skills", "Skills is required").isEmpty(),
+      check("status", "Status is required").not().isEmpty(),
+      check("skills", "Skills is required").not().isEmpty(),
     ],
   ],
   async (req, res) => {
@@ -45,31 +45,54 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    // destructure the request
     const {
+      company,
       website,
+      location,
+      bio,
+      status,
+      githubusername,
       skills,
       youtube,
+      facebook,
       twitter,
       instagram,
       linkedin,
-      facebook,
-      // spread the rest of the fields we don't need to check
-      ...rest
     } = req.body;
 
-    // build a profile
-    const profileFields = {
-      user: req.user.id,
-      website:
-        website && website !== ""
-          ? normalize(website, { forceHttps: true })
-          : "",
-      skills: Array.isArray(skills)
-        ? skills
-        : skills.split(",").map((skill) => " " + skill.trim()),
-      ...rest,
-    };
+    // Build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (skills) {
+      profileFields.skills = skills.split(",").map((skill) => skill.trim());
+    }
+
+    // Build social object
+    profileFields.social = {};
+    if (youtube) profileFields.social.youtube = youtube;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (instagram) profileFields.social.instagram = instagram;
+
+    try {
+      // Using upsert option (creates new doc if no match is found):
+      let profile = await Profile.findOneAndUpdate(
+        { user: req.user.id },
+        { $set: profileFields },
+        { new: true, upsert: true }
+      );
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
   }
 );
 
